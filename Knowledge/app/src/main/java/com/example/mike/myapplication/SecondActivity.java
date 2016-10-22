b20b5c3af947006b8fb896415918131c01d4cdd2
mike\mike <mike841112@yahoo.com.tw>
2016-10-22 14:04:09 +0800

    1.新增printOption的method，用於顯示換題目時，題目名稱 選項更動 2.新增Radio Button用於做選項 3.更新Button事件觸發，並完成切換功能

--- Knowledge/app/src/main/java/com/example/mike/myapplication/SecondActivity.java
+++ Knowledge/app/src/main/java/com/example/mike/myapplication/SecondActivity.java
@@ -5,29 +5,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import java.util.ArrayList;
 import java.util.Random;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 
 public class SecondActivity extends AppCompatActivity {
 
-    ArrayList<Question> questions = new ArrayList<Question>();
+    private ArrayList<Question> questions = new ArrayList<Question>();
+    protected Random ran = new Random();
+    protected long reciprocal;
+    protected int firstNumber;
+    protected TextView questionName;
+    protected TextView answerTA;
+    protected RadioButton answerRA;
+    protected TextView answerTB;
+    protected RadioButton answerRB;
+    protected TextView answerTC;
+    protected RadioButton answerRC;
+    protected TextView answerTD;
+    protected RadioButton answerRD;
+    protected TextView timeCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Random ran = new Random();
-        int firstNumber = ran.nextInt(questions.size());
-        Question q = new Question();
-        q = questions.get(firstNumber);
-        questions.remove(firstNumber);
-
         while(questions.isEmpty()){
-
+            Question q = new Question();
+            random(q);
+            printOption(q);
+            timerCount();
         }
+    }
+
+    protected void timerCount(){
 
         new CountDownTimer(15000, 1000){
 
@@ -37,24 +53,37 @@ public class SecondActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onTick(long millisUntilFinished) {
-
+            public void onTick(long millisUntilFinished)
+            {
+                reciprocal = millisUntilFinished/1000;
+                timeCount.setText(reciprocal + "");
             }
         }.start();
 
     }
 
+    protected void random(Question q){
 
-    protected void gamesStart(){
+        firstNumber = ran.nextInt(questions.size());
+        q = questions.get(firstNumber);
+        questions.remove(firstNumber);
 
     }
+    protected void printOption(Question q){
+        questionName.setText(q.question);
+        answerTA.setText(q.A);
+        answerTB.setText(q.B);
+        answerTC.setText(q.C);
+        answerTD.setText(q.D);
+    }
 }
-
 class Question {
+
     String question;
     String A;
     String B;
     String C;
     String D;
     String correctAnswer;
+
 }
